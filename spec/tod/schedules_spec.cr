6c007f34c11ce675dc109aca474390dfccdc6f00
Lawrence Wakefield <lawrence@iamlawrence.me>
2018-09-26 12:55:17 -0400

    Add test for first time scheduling

--- spec/tod/schedules_spec.cr
+++ spec/tod/schedules_spec.cr
@@ -7,20 +7,19 @@ require "../../src/status"
 require "../../src/task"
 require "../../src/schedules"
 
+TWO_THOUSAND = Time.new 2000, 1, 1
+
 describe Scheduling do
     it "update_schedules works correctly" do
-        two_thousand = Time.new 2000, 1, 1
         DATABASE.exec "
             insert into tasks (id, created_at, modified_at, name, schedule, status)
-            values (1, ?, ?, 'thing to do', '0 0 * * 1', #{Status::Scheduling.to_i})
-        ", two_thousand, two_thousand
-        DATABASE.exec "
-            insert into tasks (id, created_at, modified_at, name, status)
-            values (2, ?, ?, 'thing to do', #{Status::Completed.to_i})
-        ", two_thousand, two_thousand
+            values
+                (1, ?, ?, 'thing to do', '0 0 * * 1', #{Status::Scheduling.to_i}),
+                (2, ?, ?, 'thing to do', null,        #{Status::Completed.to_i})
+        ", TWO_THOUSAND, TWO_THOUSAND, TWO_THOUSAND, TWO_THOUSAND
         DATABASE.exec "insert into scheduled_tasks values (1, 2)"
 
-        Scheduling.update_schedules two_thousand + 14.days
+        Scheduling.update_schedules TWO_THOUSAND + 14.days
 
         DATABASE.query_all "select * from tasks where schedule is null" do |row|
             row.column_names.map { row.read }.join ", "
@@ -39,8 +38,31 @@ describe Scheduling do
         "
     end
 
+    it "update_schedules for the first time" do
+        DATABASE.exec "
+            insert into tasks (id, created_at, modified_at, name, schedule, status)
+            values
+                (1, ?, ?, 'thing to do', '0 0 * * 1', #{Status::Scheduling.to_i})
+        ", TWO_THOUSAND, TWO_THOUSAND
+
+        Scheduling.update_schedules TWO_THOUSAND + 14.days
+
+        DATABASE.query_all "select * from tasks where schedule is null" do |row|
+            row.column_names.map { row.read }.join ", "
+        end.join("\n").should eq "\
+            2, 2000-01-03 00:00:00.000, 2000-01-03 00:00:00.000, thing to do, 0, 0, 1, [], , \n\
+            3, 2000-01-10 00:00:00.000, 2000-01-10 00:00:00.000, thing to do, 0, 0, 1, [], , \
+        "
+
+        DATABASE.query_all "select * from scheduled_tasks" do |row|
+            row.column_names.map { row.read }.join ", "
+        end.join("\n").should eq "\
+            1, 2\n\
+            1, 3\
+        "
+    end
+
     it "get_last_scheduled_at works correctly" do
-        two_thousand = Time.new 2000, 1, 1
         DATABASE.exec("
             insert into tasks (id, created_at, modified_at)
             values
@@ -48,15 +70,15 @@ describe Scheduling do
                 (2, ?, ?),
                 (3, ?, ?)
         ",
-            two_thousand, two_thousand,
-            two_thousand + 1.day, two_thousand + 1.day,
-            two_thousand + 2.day, two_thousand + 2.day,
+            TWO_THOUSAND, TWO_THOUSAND,
+            TWO_THOUSAND + 1.day, TWO_THOUSAND + 1.day,
+            TWO_THOUSAND + 2.day, TWO_THOUSAND + 2.day,
         )
         DATABASE.exec "
             insert into scheduled_tasks (parent_id, child_id)
             values (1, 1), (1, 2), (1, 3)
         "
 
-        Scheduling.get_last_scheduled_at(1).should eq two_thousand + 2.day
+        Scheduling.get_last_scheduled_at(1).should eq TWO_THOUSAND + 2.day
     end
 end
